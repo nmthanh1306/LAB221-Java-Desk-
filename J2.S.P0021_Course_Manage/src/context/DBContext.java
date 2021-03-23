7703af9a1847370acdb79f2441e379796054bd23
nmthanh1306 <nmthanh1306@gmail.com>
2021-03-23 20:53:27 +0700

    first

--- J2.S.P0021_Course_Manage/src/context/DBContext.java
+++ J2.S.P0021_Course_Manage/src/context/DBContext.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  * @author Minh Thanh
  */
 public class DBContext {
-
+    
     public Connection getConnection() throws Exception {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String url = ("jdbc:sqlserver://"
